1523f6dc5413b3497cf74160fc9cfade89f7dcdd
itay.giladigit config --global user.email itay.giladi@post.runi.ac.ilgit config --global user.name Your <you@example.com>
2024-03-17 22:04:40 +0200

    v2

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,32 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+	String word = arg [0];
+	int wrd_length = word.length();
+	int middle_chr =0;
+	if (wrd_length == 0){
+		system.out.prntln (" ");
+		else if (wrd_length%2==0){
+			//get the mid char
+			middle_chr =(wrd_length/2)-1;
+		}else if (wrd_length%2!=0){
+			get the mid char if odd
+			middle_chr = (wrd_length/2);
+			{
+				for (int i =(word.length()-1); i>=0;' i--'){
+					system.out.print(word.charAt(i));
+					{system.out.println();
+					System.out.println("The middle character is " + word.charAt(middle_chr));
+
 	}
 }
+					}
+				}
+			}
+		}
+		}
+
+
+
+	
+
